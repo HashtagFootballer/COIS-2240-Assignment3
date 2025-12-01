5618e99e7dc855fedd438feda1fe893b0b0b5189
HashtagFootballer <90515299+HashtagFootballer@users.noreply.github.com>
2025-11-30 20:45:52 -0500

    task 1.5
    - made the capitalize() function using refactoring (as done in task 1.1)
    - deleted repetitive code

--- Vehicle.java
+++ Vehicle.java
@@ -8,20 +8,19 @@ public abstract class Vehicle {
     public enum VehicleStatus { Available, Held, Rented, UnderMaintenance, OutOfService }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
-    	
-        this.year = year;
+    	this.make = capitalize(make);//added for task 1.5
+    	this.model = capitalize(model);//added for task 1.5
+    	this.year = year;
         this.status = VehicleStatus.Available;
         this.licensePlate = null;
     }
+//Edited as per task 1.5
+	private String capitalize(String input) {//deletes use of repeated logic
+		if (input == null || input.isEmpty())
+    		return null;
+    	else
+    		return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+	}
 
     public Vehicle() {
         this(null, null, 0);
